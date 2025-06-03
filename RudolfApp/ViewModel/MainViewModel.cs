ded18a05eb6fe459ba9e3e81f59e532bad696179
LimKyeongmuk <koungmuk1975@gmail.com>
2025-06-03 13:53:53 +0900

    Feat: MainViewModel 초기 버전

---
+++ RudolfApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.RightsManagement;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace RudolfApp.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ImageSource _inputImage;
+        public ImageSource InputImage
+        {
+            get => _inputImage;
+            set
+            {
+                if (_inputImage != value)
+                {
+                    _inputImage = value;
+                    OnPropertyChanged(nameof(InputImage));
+                }
+            }
+        }
+    }
+}
