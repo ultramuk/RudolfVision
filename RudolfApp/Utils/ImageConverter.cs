ce529c3940416004d21d8da04a20fd49f13489ae
LimKyeongmuk <koungmuk1975@gmail.com>
2025-06-03 15:10:31 +0900

    Feat: OpenCvSharp Mat -> WPF ImageSource 변환 유틸 구현

---
+++ RudolfApp/Utils/ImageConverter.cs
@@ -0,0 +1,19 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System.Windows.Media;
+
+namespace RudolfApp.Utils
+{
+    public static class ImageConverter
+    {
+        /// <summary>
+        /// OpenCvSharp의 Mat 객체를 WPF에서 사용 가능한 ImageSource로 변환
+        /// </summary>
+        /// <param name="mat">OpenCvSharp Mat (BGR 이미지)</param>
+        /// <returns>WPF용 ImageSource 객체</returns>
+        public static ImageSource MatToImageSource(Mat mat)
+        {
+            return mat.ToWriteableBitmap();
+        }
+    }
+}
