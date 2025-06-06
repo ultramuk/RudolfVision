2034e0fccd8ac20f29d8875d81ffe6559a0cd844
LimKyeongmuk <koungmuk1975@gmail.com>
2025-06-06 09:38:21 +0900

    Feat: C# -> C++ DLL로 넘겨주는 중간 처리자 서비스 추가

---
+++ RudolfApp/Services/ImageProcessingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenCvSharp;
+using RudolfApp.Services.Interop;
+
+namespace RudolfApp.Services
+{
+    public class ImageProcessingService
+    {
+        public void ProcessSampleImage()
+        {
+            using var mat = Cv2.ImRead("Asserts/sample.png", ImreadModes.Color);
+            IntPtr dataPtr = mat.Data;
+
+            RudolfInterop.Initialize();
+            RudolfInterop.ProcessImage(dataPtr, mat.Width, mat.Height, mat.Channels());
+            RudolfInterop.Release();
+        }
+    }
+}
