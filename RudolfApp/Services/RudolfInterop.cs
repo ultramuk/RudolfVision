3bf56f32e6957a1e460de80812e4f84f428dee82
LimKyeongmuk <koungmuk1975@gmail.com>
2025-06-06 09:37:13 +0900

    Feat: 외부 DLL 함수 연결

---
+++ RudolfApp/Services/RudolfInterop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RudolfApp.Services.Interop
+{
+    public static class RudolfInterop
+    {
+        [DllImport("RudolfLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Initialize();
+
+        [DllImport("RudolfLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Release();
+
+        [DllImport("RudolfLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ProcessImage(IntPtr data, int width, int height, int channels);
+    }
+}
